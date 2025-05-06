8dd614daeae2159d3b42ce38f5dd3a8856e60ca2
Lina Martinez <lmcastro3501@gmail.com>
2025-05-06 16:28:47 -0700

    creates Task Model with its helper methods.

--- app/models/task.py
+++ app/models/task.py
@@ -8,3 +8,21 @@ class Task(db.Model):
     title: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
     completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
+
+    # Instance methods
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "title": self.title,
+            "is_complete": True if self.completed_at else False
+        }
+    
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=task_data.get("completed_at")  # This will be None if not provided
+                        )
+        return new_task
