a6bea7691616affab3e36d2d5ccbf7a14bf2bf68
Lina Martinez <lmcastro3501@gmail.com>
2025-05-07 23:24:29 -0700

    Define goal_id FK in Taks model - one goal to many tasks relationship.

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,17 +1,24 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 from ..db import db
 
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
+    tasks: Mapped[list["Task"]] = relationship(back_populates="goal")
 
 
     def to_dict(self):
-        return {
+        goal_dict = {
             "id": self.id,
-            "title": self.title
+            "title": self.title,
+            # "tasks": [task.to_dict() for task in self.tasks]
         }
+        
+        if self.tasks:
+            goal_dict["tasks"] = [task.to_dict() for task in self.tasks]
+        
+        return goal_dict
     
 
     @classmethod
--- app/models/task.py
+++ app/models/task.py
@@ -1,22 +1,32 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 from datetime import datetime
 from typing import Optional
+
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
-    # Instance methods
+
     def to_dict(self):
-        return {
+        task_dict = {
             "id": self.id,
             "description": self.description,
             "title": self.title,
-            "is_complete": self.completed_at is not None
+            "is_complete": self.completed_at is not None,
+            # "goal_id": self.goal.id if self.goal_id else None
         }
+
+        if self.goal_id:
+            task_dict["goal_id"] = self.goal_id
+        
+        return task_dict
     
 
     @classmethod
